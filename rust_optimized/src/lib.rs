178cf78553048b94c6ae0d0c8ec8e3c22fcf7ab3
Razvan Filea <razvan.filea@gmail.com>
2024-10-24 13:53:51 +0300

    Add auto generated math into Rust

--- rust_optimized/src/lib.rs
+++ rust_optimized/src/lib.rs
@@ -1,4 +1,11 @@
+use pyo3::prelude::*;
+use crate::spatial_grid::SpatialGrid;
+
+mod spatial_grid;
+mod spiral;
+
 #[pymodule]
 fn rust_optimized(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<SpatialGrid>()?;
     Ok(())
 }
